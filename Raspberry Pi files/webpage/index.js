f13c5e945bd7f4683fe2e68d4ae10e7e47ff9968
Viperinius <41393831+Viperinius@users.noreply.github.com>
2019-08-19 23:02:18 +0200

    first webpage ideas
    Signed-off-by: Viperinius <41393831+Viperinius@users.noreply.github.com>

--- Raspberry Pi files/webpage/index.js
+++ Raspberry Pi files/webpage/index.js
@@ -3,3 +3,16 @@
     Created:    18.08.2019
     Author:     Viperinius
 */
+
+var board;
+
+//create chess board
+//$(document).ready(function () {
+    board = Chessboard('chessBoard', 'start');
+//});
+
+
+// doesnt work yet
+$('#resetBoard').on('click', board.start);
+
+$('#clear').on('click', board.clear);
