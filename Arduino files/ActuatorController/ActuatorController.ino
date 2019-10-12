92ab2215fb541bca9e405604d9d3aa70ecec5ec4
Viperinius <41393831+Viperinius@users.noreply.github.com>
2019-10-12 14:07:27 +0200

    added arduino code documentation, added end stop implementation, fixed readSerial
    Signed-off-by: Viperinius <41393831+Viperinius@users.noreply.github.com>

--- Arduino files/ActuatorController/ActuatorController.ino
+++ Arduino files/ActuatorController/ActuatorController.ino
@@ -1,15 +1,129 @@
-/*
- Name:		ActuatorController.ino
- Created:	18.08.2019 22:04:10
- Author:	Viperinius
-*/
+//==============================================================================
+/// @file ActuatorController.ino
+///
+/// @brief Implements movement control
+///
+/// @version 1.1
+///
+/// @author Viperinius
+///
+/// @date 11.10.2019
+//==============================================================================
 
-// the setup function runs once when you press reset or power the board
-void setup() {
 
+//------------------------------------------------------------------------------
+//									Includes
+//------------------------------------------------------------------------------
+
+#include <avr/interrupt.h>
+
+
+//------------------------------------------------------------------------------
+//									Defines
+//------------------------------------------------------------------------------
+
+
+#define BAUD_RATE 9600							///< set the baud raute used to communicate
+#define SLEEP_MS 500							///< set how much delay is in between loop cycles
+#define COMM_READY_MSG "<SENSORS_COMM_READY>"	///< message to send that communication is ready
+
+#define PIN_END_STOP_1 10						///< pin of the x end stop
+#define PIN_END_STOP_2 11						///< pin of the y end stop
+#define PIN_END_STOP_3 12						///< pin of the z end stop
+
+//------------------------------------------------------------------------------
+//									Variables
+//------------------------------------------------------------------------------
+
+volatile uint8_t uiIsrValue;					///< holds value if or which end stop triggered an interrupt
+
+
+
+//------------------------------------------------------------------------------
+//									Functions
+//------------------------------------------------------------------------------
+
+//==============================================================================
+/// @brief Initialise the end stop pins
+///
+/// @details
+/// Set pin mode
+//==============================================================================
+void initEndStops() {
+	pinMode(PIN_END_STOP_1, INPUT);
+	pinMode(PIN_END_STOP_2, INPUT);
+	pinMode(PIN_END_STOP_3, INPUT);
 }
 
-// the loop function runs over and over again until power down or reset
+//==============================================================================
+/// @brief Initialise the PCI
+///
+/// @details
+/// Enables pin change interrupts for the end stops
+//==============================================================================
+void initPCI() {
+
+	uiIsrValue = 0;
+
+	cli();
+	// enable port B PCI
+	PCICR |= 0b00000001;
+	// enable interrupt on D10, D11 and D12
+	PCMSK0 |= 0b00011100;
+
+	sei();
+}
+
+
+
+
+//------------------------------------------------------------------------------
+//									Main Functions
+//------------------------------------------------------------------------------
+
+//==============================================================================
+/// @brief Prepare loop
+///
+/// @details
+/// Initialises needed things
+//==============================================================================
+void setup() {
+	Serial.begin(BAUD_RATE);
+	pinMode(LED_BUILTIN, OUTPUT);
+	Serial.println(COMM_READY_MSG);
+
+	initPCI();
+	initEndStops();
+	
+}
+
+//==============================================================================
+/// @brief Main loop
+///
+/// @details
+/// Main loop
+//==============================================================================
 void loop() {
-  
+	
+
+
+
+
+
+	delayMicroseconds(SLEEP_MS);
+}
+
+
+
+ISR(PCINT0_vect) {
+	// sets bits according to end stop states:
+	// 0 -> no end stop active
+	// 1 -> end stop 1 triggered
+	// 2 -> end stop 2 triggered
+	// 3 -> end stop 1 and 2 active
+	// 4 -> end stop 3 triggered
+	// 5 -> end stop 1 and 3 active
+	// 6 -> end stop 2 and 3 triggered
+	// 7 -> all end stops active
+	uiIsrValue = !digitalRead(PIN_END_STOP_1) | (!digitalRead(PIN_END_STOP_2) << 1) | (!digitalRead(PIN_END_STOP_3) << 2);
 }
--- Arduino files/SensorController/SensorController.ino
+++ Arduino files/SensorController/SensorController.ino
@@ -1,8 +1,15 @@
-/*
- Name:		SensorController.ino
- Created:	18.08.2019 22:03:05
- Author:	Viperinius
-*/
+//==============================================================================
+/// @file SensorController.ino
+///
+/// @brief Implements reed switch reading and move recognition
+///
+/// @version 1.2
+///
+/// @author Viperinius
+///
+/// @date 11.10.2019
+//==============================================================================
+
 
 /* Board:
 	x  x  x  x  x  x  x  x	8	|			MCP78	Pins 8-15
@@ -19,46 +26,47 @@
 
 
 
-//-----------------------------------------
-//				Includes
-//-----------------------------------------
+//------------------------------------------------------------------------------
+//									Includes
+//------------------------------------------------------------------------------
 
 #include <Wire.h>
 #include <Adafruit_MCP23017.h>
 
 
 
-//-----------------------------------------
-//				Defines
-//-----------------------------------------
-#define DEBUG 1
+//------------------------------------------------------------------------------
+//									Defines
+//------------------------------------------------------------------------------
+#define DEBUG 1									///< enables or disables certain code for debugging
 
 
-#define BAUD_RATE 9600
-#define SLEEP_MS 1500
-#define SLEEP_MS_NEW_GAME 90000	// = 1.5 min
-#define REQUEST_POS 10
-#define REQUEST_NEW_GAME 20
-#define MODE_BOARD 1
-#define MODE_WEB 2
+#define BAUD_RATE 9600							///< set the baud raute used to communicate
+#define SLEEP_MS 1500							///< set how much delay is in between a loop cycle
+#define SLEEP_MS_NEW_GAME 90000					///< set how much time a player has between requesting a new game and the actual game start
+#define REQUEST_POS 10							///< message code for a current positions request
+#define REQUEST_NEW_GAME 20						///< message code for a new game requested
+#define MODE_BOARD 1							///< code for play mode board only
+#define MODE_WEB 2								///< code for play mode web
 
-#define REQ_POS_MSG "<SEND_CURR_POS>"
-#define REQ_NEW_GAME_MSG "<RESET_GAME>"
-#define COMM_READY_MSG "<SENSORS_COMM_READY>"
-#define MODE_MSG_BOARD "<MODE_BOARD>"
-#define MODE_MSG_WEB "<MODE_WEB>"
+#define REQ_POS_MSG "<SEND_CURR_POS>"			///< message to expect to recognise a current positions request
+#define REQ_NEW_GAME_MSG "<RESET_GAME>"			///< message to expect to recognise a new game request
+#define COMM_READY_MSG "<SENSORS_COMM_READY>"	///< message to send that communication is ready
+#define MODE_MSG_BOARD "<MODE_BOARD>"			///< message to expect to set play mode to board only
+#define MODE_MSG_WEB "<MODE_WEB>"				///< message to expect to set play mode to web
 
-//-----------------------------------------
-//				Variables
-//-----------------------------------------
+//------------------------------------------------------------------------------
+//									Variables
+//------------------------------------------------------------------------------
 
 // I/O extenders, each connected to two rows of the board
-Adafruit_MCP23017 mcpRow12;
-Adafruit_MCP23017 mcpRow34;
-Adafruit_MCP23017 mcpRow56;
-Adafruit_MCP23017 mcpRow78;
+Adafruit_MCP23017 mcpRow12;		///< I/O extender for first and second row
+Adafruit_MCP23017 mcpRow34;		///< I/O extender for third and fourth row
+Adafruit_MCP23017 mcpRow56;		///< I/O extender for fifth and sixth row
+Adafruit_MCP23017 mcpRow78;		///< I/O extender for seventh and eighth row
 
 // keep starting positions at hand for easy copying
+/// backup of the starting positions
 const int iStartPos[8][8] = {
 	{ 1,1,1,1,1,1,1,1 },
 	{ 1,1,1,1,1,1,1,1 },
@@ -71,26 +79,31 @@ const int iStartPos[8][8] = {
 };
 
 // arrays to save read positions
-int iPrevPos[8][8];
-int iCurrPos[8][8];
+int iPrevPos[8][8];		///< array to store the previous positions
+int iCurrPos[8][8];		///< array to store the current positions
 
 // status if / what serial message received
-int iSerialMsg;
+int iSerialMsg;			///< contains the message read from serial
 
 // contains detected move with board columns as numbers instead of letters
-int iMove;
+int iMove;				///< contains unformatted detected move
 
 // status if comparePositions has detected a move
-bool bMoveDetected;
+bool bMoveDetected;		///< indicates if comparePostions detected a move
 
 // "play mode", either board only (MODE_BOARD) or on web page (MODE_WEB)
-uint8_t uiMode;
+uint8_t uiMode;			///< contains current play mode
 
-//-----------------------------------------
-//				Functions
-//-----------------------------------------
+//------------------------------------------------------------------------------
+//									Functions
+//------------------------------------------------------------------------------
 
-
+//==============================================================================
+/// @brief Resets the position arrays
+///
+/// @details
+/// Fill the board position arrays with initial values
+//==============================================================================
 void resetBoardArr() {
 	for (int i = 0; i < 8; i++)
 	{
@@ -102,6 +115,12 @@ void resetBoardArr() {
 	}
 }
 
+//==============================================================================
+/// @brief Copy current position array to previous position array
+///
+/// @details
+/// Copies all current positions from their array to the previous one.
+//==============================================================================
 void copyCurrToPrevPos() {
 	for (int i = 0; i < 8; i++)
 	{
@@ -112,12 +131,32 @@ void copyCurrToPrevPos() {
 	}
 }
 
+//==============================================================================
+/// @brief Generate unformatted move
+///
+/// @details
+/// Generates the variable containing the unformatted move (move with numbers only)
+///
+/// @param [in] iSrcI	origin row
+/// @param [in] iSrcJ	origin column
+/// @param [in] iDestI	destination row
+/// @param [in] iDestJ	destination column
+///
+/// @return The unformatted move as int
+//==============================================================================
 int generateMoveVar(int iSrcI, int iSrcJ, int iDestI, int iDestJ) {
 	//returns move with columns as numbers instead of letters
 	return iSrcJ * 1000 + iSrcI * 100 + iDestJ * 10 + iDestI;
 }
 
-
+//==============================================================================
+/// @brief Check if move happened
+///
+/// @details
+/// Compares position arrays to check if a piece changed position
+///
+/// @return -1 if no move detected, unformatted move otherwise
+//==============================================================================
 int comparePositions() {
 	int iSrcI = -1;
 	int iSrcJ = -1;
@@ -154,8 +193,12 @@ int comparePositions() {
 	return -1;
 }
 
-
-
+//==============================================================================
+/// @brief Initialise the MCP23017
+///
+/// @details
+/// Initialises the four port extenders
+//==============================================================================
 void initMCP() {
 	mcpRow12.begin(1);	//I2C: 0x21
 	mcpRow34.begin(2);	//I2C: 0x22
@@ -176,6 +219,12 @@ void initMCP() {
 	}
 }
 
+//==============================================================================
+/// @brief Read all reed switches
+///
+/// @details
+/// Reads all inputs of the I/O extenders and fills position arrays
+//==============================================================================
 void readSensors() {
 	int iTemp = 0;
 	for (int i = 0; i < 8; i++)
@@ -225,17 +274,24 @@ void readSensors() {
 	}	
 }
 
-
+//==============================================================================
+/// @brief Read serial
+///
+/// @details
+/// Checks if new serial message available and reads it
+///
+/// @return -1 if no or incompatible message received, otherwise message codes
+//==============================================================================
 int readSerial() {
 	char cRead;
 	String sContent = "";
 
-	if (Serial.available()) {
+	if (!Serial.available()) {
+		return -1;
+	}
+	while (Serial.available()) {
 		cRead = Serial.read();
 		sContent.concat(cRead);
-	}
-	else {
-		return -1;
 	}
 	
 	if (sContent == REQ_POS_MSG) {
@@ -256,11 +312,16 @@ int readSerial() {
 }
 
 
-//-----------------------------------------
-//				Main Functions
-//-----------------------------------------
+//------------------------------------------------------------------------------
+//									Main Functions
+//------------------------------------------------------------------------------
 
-
+//==============================================================================
+/// @brief Prepare loop
+///
+/// @details
+/// Initialises needed things
+//==============================================================================
 void setup() {
 	Serial.begin(BAUD_RATE);
 	pinMode(LED_BUILTIN, OUTPUT);
@@ -281,6 +342,12 @@ void setup() {
 
 }
 
+//==============================================================================
+/// @brief Main loop
+///
+/// @details
+/// Main loop
+//==============================================================================
 void loop() {
 	//check if server sent a request
 	iSerialMsg = readSerial();
