b023d45078f370e665340b045ec63ae08f4fbc62
Viperinius <41393831+Viperinius@users.noreply.github.com>
2019-10-12 14:17:38 +0200

    fixed Arduino indenting
    Signed-off-by: Viperinius <41393831+Viperinius@users.noreply.github.com>

--- Arduino files/ActuatorController/ActuatorController.ino
+++ Arduino files/ActuatorController/ActuatorController.ino
@@ -12,35 +12,35 @@
 
 
 //------------------------------------------------------------------------------
-//									Includes
+//                                    Includes
 //------------------------------------------------------------------------------
 
 #include <avr/interrupt.h>
 
 
 //------------------------------------------------------------------------------
-//									Defines
+//                                    Defines
 //------------------------------------------------------------------------------
 
+#define BAUD_RATE 9600                          ///< set the baud raute used to communicate
+#define SLEEP_MS 500                            ///< set how much delay is in between loop cycles
+#define COMM_READY_MSG "<SENSORS_COMM_READY>"   ///< message to send that communication is ready
 
-#define BAUD_RATE 9600							///< set the baud raute used to communicate
-#define SLEEP_MS 500							///< set how much delay is in between loop cycles
-#define COMM_READY_MSG "<SENSORS_COMM_READY>"	///< message to send that communication is ready
-
-#define PIN_END_STOP_1 10						///< pin of the x end stop
-#define PIN_END_STOP_2 11						///< pin of the y end stop
-#define PIN_END_STOP_3 12						///< pin of the z end stop
-
-//------------------------------------------------------------------------------
-//									Variables
-//------------------------------------------------------------------------------
-
-volatile uint8_t uiIsrValue;					///< holds value if or which end stop triggered an interrupt
-
+#define PIN_END_STOP_1 10                       ///< pin of the x end stop
+#define PIN_END_STOP_2 11                       ///< pin of the y end stop
+#define PIN_END_STOP_3 12                       ///< pin of the z end stop
 
 
 //------------------------------------------------------------------------------
-//									Functions
+//                                    Variables
+//------------------------------------------------------------------------------
+
+volatile uint8_t uiIsrValue;                    ///< holds value if or which end stop triggered an interrupt
+
+
+
+//------------------------------------------------------------------------------
+//                                    Functions
 //------------------------------------------------------------------------------
 
 //==============================================================================
@@ -50,9 +50,9 @@ volatile uint8_t uiIsrValue;					///< holds value if or which end stop triggered
 /// Set pin mode
 //==============================================================================
 void initEndStops() {
-	pinMode(PIN_END_STOP_1, INPUT);
-	pinMode(PIN_END_STOP_2, INPUT);
-	pinMode(PIN_END_STOP_3, INPUT);
+    pinMode(PIN_END_STOP_1, INPUT);
+    pinMode(PIN_END_STOP_2, INPUT);
+    pinMode(PIN_END_STOP_3, INPUT);
 }
 
 //==============================================================================
@@ -63,22 +63,22 @@ void initEndStops() {
 //==============================================================================
 void initPCI() {
 
-	uiIsrValue = 0;
+    uiIsrValue = 0;
 
-	cli();
-	// enable port B PCI
-	PCICR |= 0b00000001;
-	// enable interrupt on D10, D11 and D12
-	PCMSK0 |= 0b00011100;
+    cli();
+    // enable port B PCI
+    PCICR |= 0b00000001;
+    // enable interrupt on D10, D11 and D12
+    PCMSK0 |= 0b00011100;
 
-	sei();
+    sei();
 }
 
 
 
 
 //------------------------------------------------------------------------------
-//									Main Functions
+//                                    Main Functions
 //------------------------------------------------------------------------------
 
 //==============================================================================
@@ -88,13 +88,13 @@ void initPCI() {
 /// Initialises needed things
 //==============================================================================
 void setup() {
-	Serial.begin(BAUD_RATE);
-	pinMode(LED_BUILTIN, OUTPUT);
-	Serial.println(COMM_READY_MSG);
+    Serial.begin(BAUD_RATE);
+    pinMode(LED_BUILTIN, OUTPUT);
+    Serial.println(COMM_READY_MSG);
 
-	initPCI();
-	initEndStops();
-	
+    initPCI();
+    initEndStops();
+    
 }
 
 //==============================================================================
@@ -104,26 +104,26 @@ void setup() {
 /// Main loop
 //==============================================================================
 void loop() {
-	
+    
 
 
 
 
 
-	delayMicroseconds(SLEEP_MS);
+    delayMicroseconds(SLEEP_MS);
 }
 
 
 
 ISR(PCINT0_vect) {
-	// sets bits according to end stop states:
-	// 0 -> no end stop active
-	// 1 -> end stop 1 triggered
-	// 2 -> end stop 2 triggered
-	// 3 -> end stop 1 and 2 active
-	// 4 -> end stop 3 triggered
-	// 5 -> end stop 1 and 3 active
-	// 6 -> end stop 2 and 3 triggered
-	// 7 -> all end stops active
-	uiIsrValue = !digitalRead(PIN_END_STOP_1) | (!digitalRead(PIN_END_STOP_2) << 1) | (!digitalRead(PIN_END_STOP_3) << 2);
+    // sets bits according to end stop states:
+    // 0 -> no end stop active
+    // 1 -> end stop 1 triggered
+    // 2 -> end stop 2 triggered
+    // 3 -> end stop 1 and 2 active
+    // 4 -> end stop 3 triggered
+    // 5 -> end stop 1 and 3 active
+    // 6 -> end stop 2 and 3 triggered
+    // 7 -> all end stops active
+    uiIsrValue = !digitalRead(PIN_END_STOP_1) | (!digitalRead(PIN_END_STOP_2) << 1) | (!digitalRead(PIN_END_STOP_3) << 2);
 }
--- Arduino files/SensorController/SensorController.ino
+++ Arduino files/SensorController/SensorController.ino
@@ -12,22 +12,22 @@
 
 
 /* Board:
-	x  x  x  x  x  x  x  x	8	|			MCP78	Pins 8-15
-	x  x  x  x  x  x  x  x 	7	| black		MCP78	Pins 0-7
-	x  x  x  x  x  x  x  x	6				MCP56	Pins 8-15
-	x  x  x  x  x  x  x  x	5				MCP56	Pins 0-7
-	x  x  x  x  x  x  x  x	4				MCP34	Pins 8-15
-	x  x  x  x  x  x  x  x	3				MCP34	Pins 0-7
-	x  x  x  x  x  x  x  x	2	| white		MCP12	Pins 8-15
-	x  x  x  x  x  x  x  x	1	|			MCP12	Pins 0-7
-	a  b  c  d  e  f  g  h							Pin number increased from left/a to right/h
+    x  x  x  x  x  x  x  x    8    |            MCP78   Pins 8-15
+    x  x  x  x  x  x  x  x    7    | black      MCP78   Pins 0-7
+    x  x  x  x  x  x  x  x    6                 MCP56   Pins 8-15
+    x  x  x  x  x  x  x  x    5                 MCP56   Pins 0-7
+    x  x  x  x  x  x  x  x    4                 MCP34   Pins 8-15
+    x  x  x  x  x  x  x  x    3                 MCP34   Pins 0-7
+    x  x  x  x  x  x  x  x    2    | white      MCP12   Pins 8-15
+    x  x  x  x  x  x  x  x    1    |            MCP12   Pins 0-7
+    a  b  c  d  e  f  g  h                              Pin number increased from left/a to right/h
 */
 
 
 
 
 //------------------------------------------------------------------------------
-//									Includes
+//                                    Includes
 //------------------------------------------------------------------------------
 
 #include <Wire.h>
@@ -36,66 +36,66 @@
 
 
 //------------------------------------------------------------------------------
-//									Defines
+//                                    Defines
 //------------------------------------------------------------------------------
-#define DEBUG 1									///< enables or disables certain code for debugging
+#define DEBUG 1                                 ///< enables or disables certain code for debugging
 
 
-#define BAUD_RATE 9600							///< set the baud raute used to communicate
-#define SLEEP_MS 1500							///< set how much delay is in between a loop cycle
-#define SLEEP_MS_NEW_GAME 90000					///< set how much time a player has between requesting a new game and the actual game start
-#define REQUEST_POS 10							///< message code for a current positions request
-#define REQUEST_NEW_GAME 20						///< message code for a new game requested
-#define MODE_BOARD 1							///< code for play mode board only
-#define MODE_WEB 2								///< code for play mode web
+#define BAUD_RATE 9600                          ///< set the baud raute used to communicate
+#define SLEEP_MS 1500                           ///< set how much delay is in between a loop cycle
+#define SLEEP_MS_NEW_GAME 90000                 ///< set how much time a player has between requesting a new game and the actual game start
+#define REQUEST_POS 10                          ///< message code for a current positions request
+#define REQUEST_NEW_GAME 20                     ///< message code for a new game requested
+#define MODE_BOARD 1                            ///< code for play mode board only
+#define MODE_WEB 2                              ///< code for play mode web
 
-#define REQ_POS_MSG "<SEND_CURR_POS>"			///< message to expect to recognise a current positions request
-#define REQ_NEW_GAME_MSG "<RESET_GAME>"			///< message to expect to recognise a new game request
-#define COMM_READY_MSG "<SENSORS_COMM_READY>"	///< message to send that communication is ready
-#define MODE_MSG_BOARD "<MODE_BOARD>"			///< message to expect to set play mode to board only
-#define MODE_MSG_WEB "<MODE_WEB>"				///< message to expect to set play mode to web
+#define REQ_POS_MSG "<SEND_CURR_POS>"           ///< message to expect to recognise a current positions request
+#define REQ_NEW_GAME_MSG "<RESET_GAME>"         ///< message to expect to recognise a new game request
+#define COMM_READY_MSG "<SENSORS_COMM_READY>"   ///< message to send that communication is ready
+#define MODE_MSG_BOARD "<MODE_BOARD>"           ///< message to expect to set play mode to board only
+#define MODE_MSG_WEB "<MODE_WEB>"               ///< message to expect to set play mode to web
 
 //------------------------------------------------------------------------------
-//									Variables
+//                                    Variables
 //------------------------------------------------------------------------------
 
 // I/O extenders, each connected to two rows of the board
-Adafruit_MCP23017 mcpRow12;		///< I/O extender for first and second row
-Adafruit_MCP23017 mcpRow34;		///< I/O extender for third and fourth row
-Adafruit_MCP23017 mcpRow56;		///< I/O extender for fifth and sixth row
-Adafruit_MCP23017 mcpRow78;		///< I/O extender for seventh and eighth row
+Adafruit_MCP23017 mcpRow12;        ///< I/O extender for first and second row
+Adafruit_MCP23017 mcpRow34;        ///< I/O extender for third and fourth row
+Adafruit_MCP23017 mcpRow56;        ///< I/O extender for fifth and sixth row
+Adafruit_MCP23017 mcpRow78;        ///< I/O extender for seventh and eighth row
 
 // keep starting positions at hand for easy copying
 /// backup of the starting positions
 const int iStartPos[8][8] = {
-	{ 1,1,1,1,1,1,1,1 },
-	{ 1,1,1,1,1,1,1,1 },
-	{ 0,0,0,0,0,0,0,0 },
-	{ 0,0,0,0,0,0,0,0 },
-	{ 0,0,0,0,0,0,0,0 },
-	{ 0,0,0,0,0,0,0,0 },
-	{ 1,1,1,1,1,1,1,1 },
-	{ 1,1,1,1,1,1,1,1 }
+    { 1,1,1,1,1,1,1,1 },
+    { 1,1,1,1,1,1,1,1 },
+    { 0,0,0,0,0,0,0,0 },
+    { 0,0,0,0,0,0,0,0 },
+    { 0,0,0,0,0,0,0,0 },
+    { 0,0,0,0,0,0,0,0 },
+    { 1,1,1,1,1,1,1,1 },
+    { 1,1,1,1,1,1,1,1 }
 };
 
 // arrays to save read positions
-int iPrevPos[8][8];		///< array to store the previous positions
-int iCurrPos[8][8];		///< array to store the current positions
+int iPrevPos[8][8];     ///< array to store the previous positions
+int iCurrPos[8][8];     ///< array to store the current positions
 
 // status if / what serial message received
-int iSerialMsg;			///< contains the message read from serial
+int iSerialMsg;         ///< contains the message read from serial
 
 // contains detected move with board columns as numbers instead of letters
-int iMove;				///< contains unformatted detected move
+int iMove;              ///< contains unformatted detected move
 
 // status if comparePositions has detected a move
-bool bMoveDetected;		///< indicates if comparePostions detected a move
+bool bMoveDetected;     ///< indicates if comparePostions detected a move
 
 // "play mode", either board only (MODE_BOARD) or on web page (MODE_WEB)
-uint8_t uiMode;			///< contains current play mode
+uint8_t uiMode;         ///< contains current play mode
 
 //------------------------------------------------------------------------------
-//									Functions
+//                                    Functions
 //------------------------------------------------------------------------------
 
 //==============================================================================
@@ -105,14 +105,14 @@ uint8_t uiMode;			///< contains current play mode
 /// Fill the board position arrays with initial values
 //==============================================================================
 void resetBoardArr() {
-	for (int i = 0; i < 8; i++)
-	{
-		for (int j = 0; j < 8; j++)
-		{
-			iCurrPos[i][j] = iStartPos[i][j];
-			iPrevPos[i][j] = iStartPos[i][j];
-		}
-	}
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            iCurrPos[i][j] = iStartPos[i][j];
+            iPrevPos[i][j] = iStartPos[i][j];
+        }
+    }
 }
 
 //==============================================================================
@@ -122,13 +122,13 @@ void resetBoardArr() {
 /// Copies all current positions from their array to the previous one.
 //==============================================================================
 void copyCurrToPrevPos() {
-	for (int i = 0; i < 8; i++)
-	{
-		for (int j = 0; j < 8; j++)
-		{
-			iPrevPos[i][j] = iCurrPos[i][j];
-		}
-	}
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            iPrevPos[i][j] = iCurrPos[i][j];
+        }
+    }
 }
 
 //==============================================================================
@@ -137,16 +137,16 @@ void copyCurrToPrevPos() {
 /// @details
 /// Generates the variable containing the unformatted move (move with numbers only)
 ///
-/// @param [in] iSrcI	origin row
-/// @param [in] iSrcJ	origin column
-/// @param [in] iDestI	destination row
-/// @param [in] iDestJ	destination column
+/// @param [in] iSrcI    origin row
+/// @param [in] iSrcJ    origin column
+/// @param [in] iDestI    destination row
+/// @param [in] iDestJ    destination column
 ///
 /// @return The unformatted move as int
 //==============================================================================
 int generateMoveVar(int iSrcI, int iSrcJ, int iDestI, int iDestJ) {
-	//returns move with columns as numbers instead of letters
-	return iSrcJ * 1000 + iSrcI * 100 + iDestJ * 10 + iDestI;
+    //returns move with columns as numbers instead of letters
+    return iSrcJ * 1000 + iSrcI * 100 + iDestJ * 10 + iDestI;
 }
 
 //==============================================================================
@@ -158,39 +158,39 @@ int generateMoveVar(int iSrcI, int iSrcJ, int iDestI, int iDestJ) {
 /// @return -1 if no move detected, unformatted move otherwise
 //==============================================================================
 int comparePositions() {
-	int iSrcI = -1;
-	int iSrcJ = -1;
-	int iDestI = -1;
-	int iDestJ = -1;
+    int iSrcI = -1;
+    int iSrcJ = -1;
+    int iDestI = -1;
+    int iDestJ = -1;
 
-	for (int i = 0; i < 8; i++)
-	{
-		for (int j = 0; j < 8; j++)
-		{
-			if (iCurrPos[i][j] != iPrevPos[i][j]) {
-				if (iCurrPos[i][j] == 1) {
-					//piece appeared
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            if (iCurrPos[i][j] != iPrevPos[i][j]) {
+                if (iCurrPos[i][j] == 1) {
+                    //piece appeared
 
-					iDestI = i;
-					iDestJ = j;
-				}
-				else if (iCurrPos[i][j] == 0) {
-					//piece disappeared
+                    iDestI = i;
+                    iDestJ = j;
+                }
+                else if (iCurrPos[i][j] == 0) {
+                    //piece disappeared
 
-					iSrcI = i;
-					iSrcJ = j;
-				}
-			}
-		}
-	}
+                    iSrcI = i;
+                    iSrcJ = j;
+                }
+            }
+        }
+    }
 
-	//return the move if all parts valid, else -1 (no actual move seen)
-	if (iSrcI >= 0 && iSrcJ >= 0 && iDestI >= 0 && iDestJ >= 0) {
-		bMoveDetected = true;
-		return generateMoveVar(iSrcI + 1, iSrcJ + 1, iDestI + 1, iDestJ + 1);
-	}
-	bMoveDetected = false;
-	return -1;
+    //return the move if all parts valid, else -1 (no actual move seen)
+    if (iSrcI >= 0 && iSrcJ >= 0 && iDestI >= 0 && iDestJ >= 0) {
+        bMoveDetected = true;
+        return generateMoveVar(iSrcI + 1, iSrcJ + 1, iDestI + 1, iDestJ + 1);
+    }
+    bMoveDetected = false;
+    return -1;
 }
 
 //==============================================================================
@@ -200,23 +200,23 @@ int comparePositions() {
 /// Initialises the four port extenders
 //==============================================================================
 void initMCP() {
-	mcpRow12.begin(1);	//I2C: 0x21
-	mcpRow34.begin(2);	//I2C: 0x22
-	mcpRow56.begin(3);	//I2C: 0x23
-	mcpRow78.begin(4);	//I2C: 0x24
+    mcpRow12.begin(1);    //I2C: 0x21
+    mcpRow34.begin(2);    //I2C: 0x22
+    mcpRow56.begin(3);    //I2C: 0x23
+    mcpRow78.begin(4);    //I2C: 0x24
 
-	for (int i = 0; i < 16; i++)
-	{
-		mcpRow12.pinMode(i, INPUT);
-		mcpRow34.pinMode(i, INPUT);
-		mcpRow56.pinMode(i, INPUT);
-		mcpRow78.pinMode(i, INPUT);
+    for (int i = 0; i < 16; i++)
+    {
+        mcpRow12.pinMode(i, INPUT);
+        mcpRow34.pinMode(i, INPUT);
+        mcpRow56.pinMode(i, INPUT);
+        mcpRow78.pinMode(i, INPUT);
 
-		mcpRow12.pullUp(i, HIGH);
-		mcpRow34.pullUp(i, HIGH);
-		mcpRow56.pullUp(i, HIGH);
-		mcpRow78.pullUp(i, HIGH);
-	}
+        mcpRow12.pullUp(i, HIGH);
+        mcpRow34.pullUp(i, HIGH);
+        mcpRow56.pullUp(i, HIGH);
+        mcpRow78.pullUp(i, HIGH);
+    }
 }
 
 //==============================================================================
@@ -226,52 +226,52 @@ void initMCP() {
 /// Reads all inputs of the I/O extenders and fills position arrays
 //==============================================================================
 void readSensors() {
-	int iTemp = 0;
-	for (int i = 0; i < 8; i++)
-	{
-		for (int j = 0; j < 8; j++)
-		{
-			switch (i)
-			{
-			case 0:
-				iTemp = mcpRow12.digitalRead(j);
-				break;
-			case 1:
-				iTemp = mcpRow12.digitalRead(j + 8);
-				break;
-			case 2:
-				iTemp = mcpRow34.digitalRead(j);
-				break;
-			case 3:
-				iTemp = mcpRow34.digitalRead(j + 8);
-				break;
-			case 4:
-				iTemp = mcpRow56.digitalRead(j);
-				break;
-			case 5:
-				iTemp = mcpRow56.digitalRead(j + 8);
-				break;
-			case 6:
-				iTemp = mcpRow78.digitalRead(j);
-				break;
-			case 7:
-				iTemp = mcpRow78.digitalRead(j + 8);
-				break;
-			default:
-				break;
-			}
+    int iTemp = 0;
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            switch (i)
+            {
+            case 0:
+                iTemp = mcpRow12.digitalRead(j);
+                break;
+            case 1:
+                iTemp = mcpRow12.digitalRead(j + 8);
+                break;
+            case 2:
+                iTemp = mcpRow34.digitalRead(j);
+                break;
+            case 3:
+                iTemp = mcpRow34.digitalRead(j + 8);
+                break;
+            case 4:
+                iTemp = mcpRow56.digitalRead(j);
+                break;
+            case 5:
+                iTemp = mcpRow56.digitalRead(j + 8);
+                break;
+            case 6:
+                iTemp = mcpRow78.digitalRead(j);
+                break;
+            case 7:
+                iTemp = mcpRow78.digitalRead(j + 8);
+                break;
+            default:
+                break;
+            }
 
-			// invert actual value because of pullup in mcp / reed switches gnd
-			if (iTemp == 1)
-			{
-				iCurrPos[i][j] = 0;
-			}
-			else
-			{
-				iCurrPos[i][j] = 1;
-			}
-		}
-	}	
+            // invert actual value because of pullup in mcp / reed switches gnd
+            if (iTemp == 1)
+            {
+                iCurrPos[i][j] = 0;
+            }
+            else
+            {
+                iCurrPos[i][j] = 1;
+            }
+        }
+    }    
 }
 
 //==============================================================================
@@ -283,37 +283,37 @@ void readSensors() {
 /// @return -1 if no or incompatible message received, otherwise message codes
 //==============================================================================
 int readSerial() {
-	char cRead;
-	String sContent = "";
+    char cRead;
+    String sContent = "";
 
-	if (!Serial.available()) {
-		return -1;
-	}
-	while (Serial.available()) {
-		cRead = Serial.read();
-		sContent.concat(cRead);
-	}
-	
-	if (sContent == REQ_POS_MSG) {
-		return REQUEST_POS;
-	}
-	else if (sContent == REQ_NEW_GAME_MSG) {
-		return REQUEST_NEW_GAME;
-	}
-	else if (sContent == MODE_MSG_BOARD) {
-		return MODE_BOARD;
-	}
-	else if (sContent == MODE_MSG_WEB) {
-		return MODE_WEB;
-	}
-	else {
-		return -1;
-	}
+    if (!Serial.available()) {
+        return -1;
+    }
+    while (Serial.available()) {
+        cRead = Serial.read();
+        sContent.concat(cRead);
+    }
+    
+    if (sContent == REQ_POS_MSG) {
+        return REQUEST_POS;
+    }
+    else if (sContent == REQ_NEW_GAME_MSG) {
+        return REQUEST_NEW_GAME;
+    }
+    else if (sContent == MODE_MSG_BOARD) {
+        return MODE_BOARD;
+    }
+    else if (sContent == MODE_MSG_WEB) {
+        return MODE_WEB;
+    }
+    else {
+        return -1;
+    }
 }
 
 
 //------------------------------------------------------------------------------
-//									Main Functions
+//                                    Main Functions
 //------------------------------------------------------------------------------
 
 //==============================================================================
@@ -323,21 +323,21 @@ int readSerial() {
 /// Initialises needed things
 //==============================================================================
 void setup() {
-	Serial.begin(BAUD_RATE);
-	pinMode(LED_BUILTIN, OUTPUT);
-	Serial.println(COMM_READY_MSG);
+    Serial.begin(BAUD_RATE);
+    pinMode(LED_BUILTIN, OUTPUT);
+    Serial.println(COMM_READY_MSG);
 
-	initMCP();
+    initMCP();
 
-	resetBoardArr();
+    resetBoardArr();
 
-	iSerialMsg = 0;
-	bMoveDetected = false;
-	//set default mode to board only
-	uiMode = MODE_BOARD;
+    iSerialMsg = 0;
+    bMoveDetected = false;
+    //set default mode to board only
+    uiMode = MODE_BOARD;
 
 #ifdef DEBUG
-	Serial.println("SETUP DONE");
+    Serial.println("SETUP DONE");
 #endif // DEBUG
 
 }
@@ -349,64 +349,64 @@ void setup() {
 /// Main loop
 //==============================================================================
 void loop() {
-	//check if server sent a request
-	iSerialMsg = readSerial();
-	if (iSerialMsg > 0) {
-		//valid message received
-		if (iSerialMsg == REQUEST_POS) {
-			iMove = comparePositions();
+    //check if server sent a request
+    iSerialMsg = readSerial();
+    if (iSerialMsg > 0) {
+        //valid message received
+        if (iSerialMsg == REQUEST_POS) {
+            iMove = comparePositions();
 
-			if (bMoveDetected) {
-				//send latest move
-				Serial.println(iMove);
-			}
-		}
-		else if (iSerialMsg == REQUEST_NEW_GAME) {
-			resetBoardArr();
+            if (bMoveDetected) {
+                //send latest move
+                Serial.println(iMove);
+            }
+        }
+        else if (iSerialMsg == REQUEST_NEW_GAME) {
+            resetBoardArr();
 
-			//pause loop to allow players to reposition the pieces back
-			delay(SLEEP_MS_NEW_GAME);
-		}
-		else if (iSerialMsg == MODE_BOARD || iSerialMsg == MODE_WEB) {
-			//change mode to received value
-			uiMode = iSerialMsg;
-		}
-	}
-	else
-	{
-		copyCurrToPrevPos();
+            //pause loop to allow players to reposition the pieces back
+            delay(SLEEP_MS_NEW_GAME);
+        }
+        else if (iSerialMsg == MODE_BOARD || iSerialMsg == MODE_WEB) {
+            //change mode to received value
+            uiMode = iSerialMsg;
+        }
+    }
+    else
+    {
+        copyCurrToPrevPos();
 
-		readSensors();
+        readSensors();
 
-		// testing stuff
-		Serial.println("-----------");
-		//print curr array
-		for (int i = 0; i < 8; i++)
-		{
-			for (int j = 0; j < 8; j++)
-			{
-				Serial.print(iCurrPos[i][j]);
-				Serial.print(" ");
-				if (j == 7) {
-					Serial.print("\r\n");
-				}
-			}
-		}
-		digitalWrite(LED_BUILTIN, iCurrPos[4][7]);
+        // testing stuff
+        Serial.println("-----------");
+        //print curr array
+        for (int i = 0; i < 8; i++)
+        {
+            for (int j = 0; j < 8; j++)
+            {
+                Serial.print(iCurrPos[i][j]);
+                Serial.print(" ");
+                if (j == 7) {
+                    Serial.print("\r\n");
+                }
+            }
+        }
+        digitalWrite(LED_BUILTIN, iCurrPos[4][7]);
 
-		// end testing stuff
+        // end testing stuff
 
-		if (uiMode == MODE_BOARD) {
-			iMove = comparePositions();
+        if (uiMode == MODE_BOARD) {
+            iMove = comparePositions();
 
-			if (bMoveDetected) {
-				//send latest move
-				Serial.println(iMove);
-			}
-		}
+            if (bMoveDetected) {
+                //send latest move
+                Serial.println(iMove);
+            }
+        }
 
 
-		delay(SLEEP_MS);
-	}
+        delay(SLEEP_MS);
+    }
 
 }
